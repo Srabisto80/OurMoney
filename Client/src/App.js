a726b1604c719bed39fafcc02ed0c3b739ef9b71
Srabisto80 <120586224+Srabisto80@users.noreply.github.com>
2023-01-25 23:43:16 -0700

    "update"

--- Client/src/App.js
+++ Client/src/App.js
@@ -26,7 +26,7 @@ import Login from './Components/login/Login';
 import ForgotPass from './Components/forgotpass/ForgotPass';
 import Register from './Components/register/Register';
 import AddIncome from './Components/addincome/AddIncome';
-// import AddBill from './Components/addBill/AddBill';
+import AddBill from './Components/addbill/AddBill';
 import Dashboard from './Components/dashboard/Dashboard';
 
 
@@ -38,7 +38,7 @@ function App() {
             <Route path="/ForgotPass" element={<ForgotPass />} />
             <Route path="/Register" element={<Register />} />
             <Route path="/AddIncome" element={<AddIncome />} />
-            {/* <Route path="/AddBill" element={<AddBill />} /> */}
+            <Route path="/AddBill" element={<AddBill />} />
             <Route path="/Dashboard" element={<Dashboard />} />
         </Routes>
     </div>
--- Client/src/Components/dashboard/Dashboard.jsx
+++ Client/src/Components/dashboard/Dashboard.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import React, { useState } from "react";
 import "./Dashboard.css";
 // import logo from "../../moneyManLogo1.png";
 // import { Link } from "react-router-dom";
-// import axios from "axios";
+import axios from "axios";
 // import { useNavigate } from "react-router-dom" 
 
 
-const Dashboard = async (e) => {
+const Dashboard = (e) => {
     // e.preventDefault();
     console.log(localStorage.getItem('email'))
-//    const userEmail = localStorage.getItem('email')
-//     console.log(userEmail);
+   const userEmail = localStorage.getItem('email')
+    console.log(userEmail);
 
+   useEffect(() => {
+    console.log("use effect")
+    axios
+        .post("http://localhost:3001/api/Dash", { userEmail })
+        .then((res) => {
+            console.log(res)
+            
+        })
+    }, [])
 
-
+    
     return (
         <div>
             <h1>Dashboard</h1>
